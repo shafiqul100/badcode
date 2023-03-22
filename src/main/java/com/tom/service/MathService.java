f69e2d2c7103d52b697cd53aca527f25c32dd766
shafiqul100 <md.shafiqul.islam@bjitgroup.com>
2023-03-22 11:35:39 +0600

    wrong 2 added

--- src/main/java/com/tom/service/MathService.java
+++ src/main/java/com/tom/service/MathService.java
@@ -9,6 +9,9 @@ public class MathService {
     public int subtract(int firstNumber, int secondNumber) {
         Integer myInt = null; 1
         Integer yourInt = myInt * 2;
+        Integer yourInt = myInt * 2;
+        Integer yourInt = myInt * 2;
+        Integer yourInt = myInt * 2;
         return firstNumber - secondNumber;
     }
 }
